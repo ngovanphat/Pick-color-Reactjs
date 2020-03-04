d06b486b5d8707329ddd5db11cc7467281016058
ngovanphat <1753082@student.hcmus.edu.vn>
2020-03-04 16:00:48 +0700

    Complete font-end

--- src/App.js
+++ src/App.js
@@ -5,20 +5,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="row m-1">
+       <div className="col-6">
+         <div className="jumbotron jumbotron-fluid">
+           <div className="container">
+             <h1 className="display-3">Color Picker</h1>
+             <p className="lead">Choose one option</p>
+           </div>
+         </div>
+       </div>
+       <div className="col-6">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-3">Size picker</h1>
+            <p className="lead">Choose Size</p>
+            <p className="display-6">Size: 5</p>
+            <button type="button" className="btn  bg-light m-1">Increase</button>
+            <button type="button" className="btn  bg-light m-1">Decrease</button>
+          </div>
+        </div>
+       </div>
+     </div>
+     <div className="row mb-1 justify-content-center">
+     <button type="button" className="btn bg-primary w-25">Reset</button>
+     </div>
+    <div  className Name="row m-1">
+      <div className="col-12">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-3">Result</h1>
+            <p className="lead">Color:  Red - FontSize: 15px</p>
+            <hr className="my-2"/>
+            <p className="border border-primary p-2">Content setting</p>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
